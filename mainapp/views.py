31edff45e003ad0d90a9e7101024f17460a1b7bc
denis-volkov <denis-not@ya.ru>
2019-05-28 13:20:48 +0300

    Ссылки в html и css поправлены в соответствии с новой структурой

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+
+def azimut_80(request):
+    return render(request, 'mainapp/products/Azimut 80.html')
+
+
+def azimut_120sl(request):
+    return render(request, 'mainapp/products/Azimut 120SL.html')
+
+
+def benetti_veloce_140(request):
+    return render(request, 'mainapp/products/Benetti Veloce 140.html')
